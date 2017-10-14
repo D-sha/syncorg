7fd47520d04ca211145d7419fbff1e433dc73d1f
D-sha <deanshannon3@gmail.com>
2017-10-14 21:56:40 +1000

    Disable unused mockito
    Comment unused imports from currently empty unit test

--- SyncOrg/src/test/java/SimpleTests.java
+++ SyncOrg/src/test/java/SimpleTests.java
@@ -2,12 +2,12 @@ import android.content.Context;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
+//import org.mockito.Mock;
+//import org.mockito.junit.MockitoJUnit;
+//import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.verify;
 
 public class SimpleTests {
 
