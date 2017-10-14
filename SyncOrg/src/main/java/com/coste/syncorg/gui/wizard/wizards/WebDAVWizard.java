b70d362db793a198d12d4aa02ee0b9ca201ba885
D-sha <deanshannon3@gmail.com>
2017-10-14 22:01:33 +1000

    Enable Webdav wizard
    Enable radiobutton, give the button a string
    Uncomment webdav code, convert login test code to async task and save settings

--- SyncOrg/src/main/java/com/coste/syncorg/gui/wizard/wizards/WebDAVWizard.java
+++ SyncOrg/src/main/java/com/coste/syncorg/gui/wizard/wizards/WebDAVWizard.java
@@ -1,15 +1,24 @@
 package com.coste.syncorg.gui.wizard.wizards;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.coste.syncorg.R;
+import com.coste.syncorg.synchronizers.WebDAVSynchronizer;
+
+import static com.coste.syncorg.settings.SettingsActivity.KEY_SYNC_SOURCE;
 
 public class WebDAVWizard extends AppCompatActivity {
 
+    private static final String TAG = "syncorg";
     private EditText webdavUser;
     private EditText webdavPass;
     private EditText webdavUrl;
@@ -26,49 +35,50 @@ public class WebDAVWizard extends AppCompatActivity {
         webdavLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//				loginWebdav();
+				loginWebdav();
             }
         });
     }
 
+	public void loginWebdav() {
+		final String urlActual = webdavUrl.getText().toString();
+		final String passActual = webdavPass.getText().toString();
+		final String userActual = webdavUser.getText().toString();
 
-//	public void loginWebdav() {
-//		final String urlActual = webdavUrl.getText().toString();
-//		final String passActual = webdavPass.getText().toString();
-//		final String userActual = webdavUser.getText().toString();
-//		progress.show();
-//
-//		Thread uiThread = new HandlerThread("UIHandler");
-//		uiThread.start();
-//		uiHandler = new UIHandler(((HandlerThread) uiThread).getLooper());
-//
-//		Thread loginThread = new Thread() {
-//			public void run() {
-//				WebDAVSynchronizer wds = new WebDAVSynchronizer(context);
-//				String extra = wds.testConnection(urlActual, userActual,
-//						passActual);
-//				if (extra != null) {
-//					showToastRemote("Login failed: " + extra);
-//					return;
-//				}
-//				showToastRemote("Login succeeded");
-//			}
-//		};
-//		loginThread.start();
-//	}
-//
-//	public void saveSettings() {
-//		SharedPreferences appSettings = PreferenceManager
-//				.getDefaultSharedPreferences(context);
-//		SharedPreferences.Editor editor = appSettings.edit();
-//
-//		editor.putString(KEY_SYNC_SOURCE, "webdav");
-//
-//		editor.putString("webUrl", webdavUrl.getText().toString());
-//		editor.putString("webPass", webdavPass.getText().toString());
-//		editor.putString("webUser", webdavUser.getText().toString());
-//
-//		editor.apply();
-//		((Activity) context).finish();
-//	}
+        final Context context = getApplicationContext();
+
+		class Atask extends AsyncTask<String, Void, String> {
+            protected String doInBackground(String... params) {
+                WebDAVSynchronizer wds = new WebDAVSynchronizer(context);
+                return wds.testConnection(urlActual, userActual, passActual);
+            }
+
+            @Override
+            protected void onPostExecute(String testResult) {
+                if (testResult != null){
+                    Toast.makeText(WebDAVWizard.this, "Login failed: " + testResult, Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(WebDAVWizard.this, "Login succeeded", Toast.LENGTH_LONG).show();
+                    saveSettings();
+                }
+            }
+        }
+        new Atask().execute("");
+
+    }
+
+	public void saveSettings() {
+		SharedPreferences appSettings = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor editor = appSettings.edit();
+
+		editor.putString(KEY_SYNC_SOURCE, "webdav");
+
+		editor.putString("webUrl", webdavUrl.getText().toString());
+		editor.putString("webPass", webdavPass.getText().toString());
+		editor.putString("webUser", webdavUser.getText().toString());
+
+		editor.apply();
+		finish();
+	}
 }
